03a28c7cfe0f0bc2c457bc8e23596eb8591496f9
AMIRIOX <wumingyun2120@outlook.com>
2025-08-17 17:00:19 +0800

    [fin] cache lab [53/53]

--- cachelab/trans.c
+++ cachelab/trans.c
@@ -20,6 +20,11 @@ int is_transpose(int M, int N, int A[N][M], int B[M][N]);
  *     be graded.
  */
 int min(int a, int b) { return a <= b ? a : b; }
+void swap(int *a, int *b) {
+    int tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
 char transpose_submit_desc[] = "Transpose submission";
 void transpose_submit(int M, int N, int A[N][M], int B[M][N]) {
     /*
@@ -90,36 +95,179 @@ void transpose_submit(int M, int N, int A[N][M], int B[M][N]) {
         }
 #undef BSIZE
     } else {
+        // 64 * 64 的矩阵, A[i] 和 A[i+4] 会在同一组导致冲突
+        // !!! 但是 BSIZE = 4 的方案使得一个缓存行(8个整数)只能有效利用一半(4个)
+        // 所以还是要考虑 BSIZE = 8 的方案
+#define BSIZE 8
+        int a, b, c, d, e, f, g, h;
+        for (int kk = 0; kk < N; kk += BSIZE) {
+            for (int jj = 0; jj < M; jj += BSIZE) {
+                // 还是 8 * 8, 但是要对每一分块再分块
+
+                // 我操, 写错了, A 的 (kk, jj) 块要转置到 B 的 (jj, kk) 块
+                // OK 改好了
+
+                // 先复制前四行, 因为第五行会出现 A[0] 和 A[4] 冲突
+                for (int i = 0; i < 4; i++) {
+                    a = A[kk + i][jj + 0];
+                    b = A[kk + i][jj + 1];
+                    c = A[kk + i][jj + 2];
+                    d = A[kk + i][jj + 3];
+                    e = A[kk + i][jj + 4];
+                    f = A[kk + i][jj + 5];
+                    g = A[kk + i][jj + 6];
+                    h = A[kk + i][jj + 7];
+
+                    B[jj + 0][kk + i] = a;
+                    B[jj + 1][kk + i] = b;
+                    B[jj + 2][kk + i] = c;
+                    B[jj + 3][kk + i] = d;
+                    B[jj + 0][kk + 4 + i] = e;
+                    B[jj + 1][kk + 4 + i] = f;
+                    B[jj + 2][kk + 4 + i] = g;
+                    B[jj + 3][kk + 4 + i] = h;
+                }
+                // A 的前四行, B 的前四行都在缓存里
+                // A 的前四行已经没用了, 可以直接踢出
+                for (int i = 0; i < 4; i++) {
+                    // 缓冲 B 的右上角(此时位置错误)
+                    // hit B 第 i 行一行后半的缓存
+                    a = B[jj + i][kk + 4 + 0];
+                    b = B[jj + i][kk + 4 + 1];
+                    c = B[jj + i][kk + 4 + 2];
+                    d = B[jj + i][kk + 4 + 3];
+
+                    // A 的左下角转置并正确放置到 B 的右上角
+                    // evict A 前四行的缓存, 加载/hit 后四行
+                    // hit B 第 i 行一行后半的缓存
+                    e = A[kk + 4 + 0][jj + i];
+                    f = A[kk + 4 + 1][jj + i];
+                    g = A[kk + 4 + 2][jj + i];
+                    h = A[kk + 4 + 3][jj + i];
+                    B[jj + i][kk + 4] = e;
+                    B[jj + i][kk + 4 + 1] = f;
+                    B[jj + i][kk + 4 + 2] = g;
+                    B[jj + i][kk + 4 + 3] = h;
+
+                    // 此时 B 的第 i 行缓存已经没用了, 可以直接踢出
+
+                    // 恢复 B 的右上角到正确的位置(左下)
+                    // evict B 第 i 行的缓存, 加载第 i + 4 行
+                    B[jj + i + 4][kk + 0] = a;
+                    B[jj + i + 4][kk + 1] = b;
+                    B[jj + i + 4][kk + 2] = c;
+                    B[jj + i + 4][kk + 3] = d;
+                }
+                // 还剩 B 的右下角
+                for (int i = 0; i < 4; i++) {
+                    // A 的下半部分第 i 行
+                    a = A[kk + 4 + i][jj + 4 + 0];
+                    b = A[kk + 4 + i][jj + 4 + 1];
+                    c = A[kk + 4 + i][jj + 4 + 2];
+                    d = A[kk + 4 + i][jj + 4 + 3];
+                    // B 的下半部分 4 行
+                    B[jj + 4 + 0][kk + 4 + i] = a;
+                    B[jj + 4 + 1][kk + 4 + i] = b;
+                    B[jj + 4 + 2][kk + 4 + i] = c;
+                    B[jj + 4 + 3][kk + 4 + i] = d;
+                }
+                // 妈的终于过了, 我快写死了
+            }
+        }
+#undef BSIZE
+
+#ifdef BSIZE4
 #define BSIZE 4
         int a, b, c, d;
         for (int kk = 0; kk < N; kk += BSIZE) {
             for (int jj = 0; jj < M; jj += BSIZE) {
                 // a chunk with left-top corner (kk, jj)
-                for (int i = jj; i < min(jj + BSIZE, M); i++) {
 
-                    // 我真希望写一些宏魔法
-                    // 但这里空白太小, 写不下 (迫真
-                    if (kk + 0 < N)
-                        a = A[kk][i];
-                    if (kk + 1 < N)
-                        b = A[kk + 1][i];
-                    if (kk + 2 < N)
-                        c = A[kk + 2][i];
-                    if (kk + 3 < N)
-                        d = A[kk + 3][i];
+                // 我真希望写一些宏魔法
+                // 但这里空白太小, 写不下 (迫真
 
-                    if (kk + 0 < N)
-                        B[i][kk] = a;
-                    if (kk + 1 < N)
-                        B[i][kk + 1] = b;
-                    if (kk + 2 < N)
-                        B[i][kk + 2] = c;
-                    if (kk + 3 < N)
-                        B[i][kk + 3] = d;
+                // 32 * 32 的矩阵在循环展开后每块只冲突一次, 规模小可以接受
+                // 64 * 64 仍然比满分要求的 misses 高了 300 多
+
+                if (kk != jj) {
+                    for (int i = jj; i < min(jj + BSIZE, M); i++) {
+                        if (kk + 0 < N)
+                            a = A[kk][i];
+                        if (kk + 1 < N)
+                            b = A[kk + 1][i];
+                        if (kk + 2 < N)
+                            c = A[kk + 2][i];
+                        if (kk + 3 < N)
+                            d = A[kk + 3][i];
+
+                        if (kk + 0 < N)
+                            B[i][kk] = a;
+                        if (kk + 1 < N)
+                            B[i][kk + 1] = b;
+                        if (kk + 2 < N)
+                            B[i][kk + 2] = c;
+                        if (kk + 3 < N)
+                            B[i][kk + 3] = d;
+                    }
+                } else {
+                    // 4 * 4 的块内, 左上, 右下的 2 * 2 块对位复制
+                    // 错位复制+转置会导致边界非常难以确定, 我脑子烧了
+                    // 但好在这是 64 * 64, 应该不会涉及太麻烦的边界问题
+                    // 但是错位转置本身就很烧脑了
+                    // kk == jj;
+                    // 。。。但是我这么写完反而更烂了, 反而低了 0.2 分
+                    // 我没招了
+                    int e, f, g, h;
+                    a = A[kk + 0][jj + 0];
+                    b = A[kk + 0][jj + 1];
+                    c = A[kk + 0][jj + 2];
+                    d = A[kk + 0][jj + 3];
+                    e = A[kk + 1][jj + 0];
+                    f = A[kk + 1][jj + 1];
+                    g = A[kk + 1][jj + 2];
+                    h = A[kk + 1][jj + 3];
+
+                    B[jj + 2][kk + 2] = a;
+                    B[jj + 3][kk + 2] = b;
+                    B[jj + 2][kk + 0] = c;
+                    B[jj + 3][kk + 0] = d;
+                    B[jj + 2][kk + 3] = e;
+                    B[jj + 3][kk + 3] = f;
+                    B[jj + 2][kk + 1] = g;
+                    B[jj + 3][kk + 1] = h;
+
+                    a = A[kk + 2][jj + 0];
+                    b = A[kk + 2][jj + 1];
+                    c = A[kk + 2][jj + 2];
+                    d = A[kk + 2][jj + 3];
+                    e = A[kk + 3][jj + 0];
+                    f = A[kk + 3][jj + 1];
+                    g = A[kk + 3][jj + 2];
+                    h = A[kk + 3][jj + 3];
+
+                    B[jj + 0][kk + 2] = a;
+                    B[jj + 1][kk + 2] = b;
+                    B[jj + 0][kk + 0] = c;
+                    B[jj + 1][kk + 0] = d;
+                    B[jj + 0][kk + 3] = e;
+                    B[jj + 1][kk + 3] = f;
+                    B[jj + 0][kk + 1] = g;
+                    B[jj + 1][kk + 1] = h;
+
+                    swap(&B[jj + 0][kk + 0], &B[jj + 2][kk + 2]);
+                    swap(&B[jj + 0][kk + 1], &B[jj + 2][kk + 3]);
+                    swap(&B[jj + 1][kk + 0], &B[jj + 3][kk + 2]);
+                    swap(&B[jj + 1][kk + 1], &B[jj + 3][kk + 3]);
+
+                    // swap(&B[jj + 0][kk + 2], &B[jj + 2][kk + 0]);
+                    // swap(&B[jj + 0][kk + 3], &B[jj + 2][kk + 1]);
+                    // swap(&B[jj + 1][kk + 2], &B[jj + 3][kk + 0]);
+                    // swap(&B[jj + 1][kk + 3], &B[jj + 3][kk + 1]);
                 }
             }
         }
 #undef BSIZE
+#endif
     }
 #ifdef DIG
     else {
