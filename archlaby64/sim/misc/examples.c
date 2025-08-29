32847a45d0c472cb94bca5518ea584f4203bb86a
AMIRIOX <wumingyun2120@outlook.com>
2025-08-29 17:41:29 +0800

    [build] fix archlab y64 processor simulator
    一些库 API 更新, 删除了 matherr
    很遗憾 CMU 15-213 把 Architecture Lab 移出了教学 Schedule

--- archlaby64/sim/misc/examples.c
+++ archlaby64/sim/misc/examples.c
@@ -1,6 +1,6 @@
-/* 
- * Architecture Lab: Part A 
- * 
+/*
+ * Architecture Lab: Part A
+ *
  * High level specs for the functions that the students will rewrite
  * in Y86-64 assembly language
  */
@@ -13,37 +13,34 @@ typedef struct ELE {
 } *list_ptr;
 
 /* sum_list - Sum the elements of a linked list */
-long sum_list(list_ptr ls)
-{
+long sum_list(list_ptr ls) {
     long val = 0;
     while (ls) {
-	val += ls->val;
-	ls = ls->next;
+        val += ls->val;
+        ls = ls->next;
     }
     return val;
 }
 
 /* rsum_list - Recursive version of sum_list */
-long rsum_list(list_ptr ls)
-{
+long rsum_list(list_ptr ls) {
     if (!ls)
-	return 0;
+        return 0;
     else {
-	long val = ls->val;
-	long rest = rsum_list(ls->next);
-	return val + rest;
+        long val = ls->val;
+        long rest = rsum_list(ls->next);
+        return val + rest;
     }
 }
 
 /* copy_block - Copy src to dest and return xor checksum of src */
-long copy_block(long *src, long *dest, long len)
-{
+long copy_block(long *src, long *dest, long len) {
     long result = 0;
     while (len > 0) {
-	long val = *src++;
-	*dest++ = val;
-	result ^= val;
-	len--;
+        long val = *src++;
+        *dest++ = val;
+        result ^= val;
+        len--;
     }
     return result;
 }
